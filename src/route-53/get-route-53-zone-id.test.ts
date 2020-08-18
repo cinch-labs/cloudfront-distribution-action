44174774c977f19d312624dfd1f3349be235440c
Bertie Blackman <blackmanrgh@gmail.com>
2020-08-18 17:41:05 +0100

    Route 53 zone name

--- src/route-53/get-route-53-zone-id.test.ts
+++
@@ -1,7 +0,0 @@
-import { getRoute53ZoneID } from './get-route-53-zone-id'
-
-describe('getRoute53ZoneID', () => {
-  it('getRoute53ZoneID', () => {
-    getRoute53ZoneID('prod.cinch.co.uk')
-  })
-})
