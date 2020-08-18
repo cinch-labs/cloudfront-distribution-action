d05757a234cfa8df47a56a98a934d29f1e116612
Bertie Blackman <blackmanrgh@gmail.com>
2020-08-18 17:36:32 +0100

    Route 53 zone name

--- src/route-53/get-route-53-zone-id.ts
+++ src/route-53/get-route-53-zone-id.ts
@@ -8,7 +8,6 @@ type GetRoute53ZoneID = (zoneName: string) => Promise<string | undefined>
 const getRoute53ZoneID: GetRoute53ZoneID = async (zoneName) => {
   try {
     const hostedZonesByName = await route53.listHostedZonesByName({ DNSName: zoneName }).promise()
-    const hostedZoneID = hostedZonesByName.HostedZones.filter((zone) => zone.Name === zoneName)[0].Id
 
     console.log('hostedZonesByName', hostedZonesByName)
 
@@ -16,6 +15,8 @@ const getRoute53ZoneID: GetRoute53ZoneID = async (zoneName) => {
       console.log('zone.Name', zone.Name)
       console.log('zoneName', zoneName)
     })
+
+    const hostedZoneID = hostedZonesByName.HostedZones.filter((zone) => zone.Name === zoneName)[0].Id
 
     if (!hostedZoneID) {
       core.setFailed('No hosted zone exists for the given Route53ZoneName')
