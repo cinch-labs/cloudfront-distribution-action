df123b2f75760cbcd2ad71f06358d301e24b4dae
Bertie Blackman <blackmanrgh@gmail.com>
2020-08-19 13:12:46 +0100

    OAI ARN

--- src/cloudformation/get-cf-stack-status.ts
+++ src/cloudformation/get-cf-stack-status.ts
@@ -9,6 +9,10 @@ const getCFStackStatus: GetCFStackStatus = async (stackName) => {
   try {
     const availableStacks = await cloudFormation.listStacks().promise()
 
+    const testing = await cloudFormation.describeStacks({ StackName: 'inventory-specdataevents-215101275-a' }).promise()
+
+    console.log('TEST', testing)
+
     console.log(stackName)
 
     console.log('availableStacks', availableStacks)
