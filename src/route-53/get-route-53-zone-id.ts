a63811f2a44d53b81ad75a7c1a3f7bf8f4423fd8
Bertie Blackman <blackmanrgh@gmail.com>
2020-08-19 10:37:17 +0100

    Route 53 zone name

--- src/route-53/get-route-53-zone-id.ts
+++ src/route-53/get-route-53-zone-id.ts
@@ -1,17 +1,17 @@
 import * as core from '@actions/core'
-import AWS from 'aws-sdk'
+import { Route53 } from 'aws-sdk'
 
-const route53 = new AWS.Route53()
+const route53 = new Route53()
 
 type GetRoute53ZoneID = (zoneName: string) => Promise<string | undefined>
 
 const getRoute53ZoneID: GetRoute53ZoneID = async (zoneName) => {
   try {
     const hostedZonesByName = await route53.listHostedZonesByName({ DNSName: zoneName }).promise()
-    const hostedZoneID = hostedZonesByName.HostedZones.filter((zone) => zone.Name === `${zoneName}.`)[0].Id
+    const hostedZoneID = hostedZonesByName.HostedZones?.filter((zone) => zone.Name === `${zoneName}.`)[0]?.Id
 
     if (!hostedZoneID) {
-      core.setFailed('No hosted zone exists for the given Route53ZoneName')
+      core.setFailed('No ID exists for the given Route53ZoneName')
     }
 
     return hostedZoneID
