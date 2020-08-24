0c6389a8009265473b750b393891bf4c9a9003a3
Bertie Blackman <blackmanrgh@gmail.com>
2020-08-24 13:37:28 +0100

    Gets a working test at last

--- src/cloudformation/get-lambda-arn.test.ts
+++ src/cloudformation/get-lambda-arn.test.ts
@@ -4,34 +4,36 @@ import { getLambdaARN } from './get-lambda-arn'
 
 jest.mock('aws-sdk')
 
+const stackName = 'stack-of-llamas'
+const lambdaARN = 'abc123'
+
+beforeEach(() => {
+  const describeStacks = jest.fn().mockReturnValue({
+    promise: jest.fn().mockResolvedValue({
+      Stacks: [
+        {
+          StackName: stackName,
+          StackStatus: 'CREATE_IN_PROGRESS',
+          CreationTime: new Date(),
+          Outputs: [
+            {
+              OutputKey: 'LambdaARN',
+              OutputValue: lambdaARN,
+            },
+          ],
+        },
+      ],
+      $response: null as any,
+    }),
+  })
+
+  ;(CloudFormation as any).mockImplementation(() => ({
+    describeStacks,
+  }))
+})
+
 describe('getLambdaARN', () => {
-  it('works ffs', async () => {
-    const stackName = 'stack-of-llamas'
-    const lambdaARN = 'abc123'
-
-    const describeStacksPromise = jest.fn().mockReturnValue({
-      promise: jest.fn().mockResolvedValue({
-        Stacks: [
-          {
-            StackName: stackName,
-            StackStatus: 'CREATE_IN_PROGRESS',
-            CreationTime: new Date(),
-            Outputs: [
-              {
-                OutputKey: 'LambdaARN',
-                OutputValue: lambdaARN,
-              },
-            ],
-          },
-        ],
-        $response: null as any,
-      }),
-    })
-
-    ;(CloudFormation as any).mockImplementation(() => ({
-      describeStacks: describeStacksPromise,
-    }))
-
+  it('returns the correct lambda arn when the given ', async () => {
     const actualResult = await getLambdaARN(stackName)
 
     const expectedResult = lambdaARN
